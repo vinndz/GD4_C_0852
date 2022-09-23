85ca5443a284416592ddba60d7087c947217cbdf
vinndz <jeremykev186@gmail.com>
2022-09-23 10:08:26 +0700

    TGS4_C_10852

--- page/editSeriesPage.php
+++ page/editSeriesPage.php
@@ -1,64 +1,81 @@
-<?php 
-    include '../component/sidebar.php';
+<?php
+include '../component/sidebar.php';
+$id = $_GET['id']; // get id through query string
 
-    $id = $_GET['id'];  
+$qry = mysqli_query($con,"select * from Series where id='$id'"); // select query
 
-    $qry = mysqli_query($con,"select * from series where id='$id'");
-
-    $data = mysqli_fetch_assoc($qry); 
-    $nameSeries = $data["name"];
-    $_SESSION['genreSeries'] =$data["genre"];
-    $realeaseSeries = $data["realease"];
-    $episodeSeries = $data["episode"];
-    $seasonSeries = $data["season"];
-    $synopsisSeries = $data["synopsis"];
+$data = mysqli_fetch_assoc($qry); // fetch data
+$nameSeries = $data["name"];
+$_SESSION['genreSeries'] =$data["genre"];
+$realeaseSeries = $data["realease"];
+$episodeSeries = $data["episode"];
+$seasonSeries = $data["season"];
+$synopsisSeries = $data["synopsis"];
 ?>
 
-    <div class="container p-3 m-4 h-100" style="background-color: #FFFFFF;  border-top: 5px solid #D40013; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-        <h4>Update Series</h4>
-        <hr>
-        <form action="../process/editSeriesProcess.php" method="post">
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Name</label>
-                <input class ="form-control" id="name" name = "name" aria-describedby="emailHelp"
-                value="<?php echo (isset($nameSeries)) ? $nameSeries: ''?>" aria-describedby="emailHelp">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Genre</label>
-                <select class ="form-select" id="genre" name = "genre" aria-describedby="Default select example" multiple>
-                    <option value="Horror">Horror</option>
-                    <option value="Fantasy">Fantasy</option>
-                    <option value="Thriller">Thriller</option>
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Release</label>
-                <input class ="form-control" id="release" name = "release" aria-describedby="emailHelp"
-                value="<?php echo (isset($realeaseSeries)) ? $realeaseSeries: ''?>" aria-describedby="emailHelp">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Episode</label>
-                <input class ="form-control" id="episode" name = "episode" aria-describedby="emailHelp"
-                value="<?php echo (isset($episodeSeries)) ? $episodeSeries: ''?>" aria-describedby="emailHelp">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Season</label>
-                <input class ="form-control" id="season" name = "season" aria-describedby="emailHelp"
-                value="<?php echo (isset($seasonSeries)) ? $seasonSeries: ''?>" aria-describedby="emailHelp">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Synopsis</label>
-                <input class ="form-control" id="synopsis" name = "synopsis" aria-describedby="emailHelp"
-                value="<?php echo (isset($synopsisSeries)) ? $synopsisSeries: ''?>" aria-describedby="emailHelp">           
-            </div>
-            <div class="d-grid gap-2">
-                <button type="submit" class="btn btn-primary" name="edit">Save</button>
-            </div>
-        </form>
+<div class="container p-3 m-4 h-100"
+  style="background-color: #FFFFFF; border-top: 5px solid #17337A; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+  <h4>Update Series</h4>
+  <hr>
+  <form action="../process/editSeriesProcess.php?id=<?php echo $id; ?>" method="post">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Name</label>
+      <input class="form-control" id="name" name="name" value="<?php echo (isset($nameSeries)) ? $nameSeries: ''?>"
+        aria-describedby="emailHelp">
     </div>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Genre</label>
+      <select class="form-select" aria-label="multiple select example" name="genre[]" id="genre" multiple value=<?php
+                    $array = array("Thriller", "Comedy", "Fantasy");
+                    session_start();
+                    $genreSeries = $_SESSION['genreSeries'];
+                    foreach($array as $value=>$name)
+                    {
+                        if($name == $genreSeries)
+                        {
+                             echo "<option selected value='".$name."'>".$name."</option>";
+                        }
+                        else
+                        {
+                             echo "<option value='".$name."'>".$name."</option>";
+                        }
+                    }
+                ?>
+                </select>
+    </div>
+
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Realease</label>
+      <input class="form-control" id="realease" name="realease"
+        value="<?php echo (isset($realeaseSeries)) ? $realeaseSeries: ''?>" aria-describedby="emailHelp">
+    </div>
+
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Episode</label>
+      <input class="form-control" id="episode" name="episode"
+        value="<?php echo (isset($episodeSeries)) ? $episodeSeries: ''?>" aria-describedby="emailHelp">
+    </div>
+
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Season</label>
+      <input class="form-control" id="season" name="season"
+        value=" <?php echo (isset($seasonSeries)) ? $seasonSeries: ''?>" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Synopsis</label>
+      <input class="form-control" id="synopsis" name="synopsis"
+        value=" <?php echo (isset($synopsisSeries)) ? $synopsisSeries: ''?>" aria-describedby="emailHelp">
+    </div>
+
+    <div class="d-grid gap-2">
+      <button type="submit" class="btn btn-primary" name="editSeries">Edit Series</button>
+    </div>
+  </form>
+</div>
 </aside>
+<!-- Option 1: Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-
-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-</script>
+MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- process/editSeriesProcess.php
+++ process/editSeriesProcess.php
@@ -1,31 +1,31 @@
 <?php
 
-if (isset($_POST['edit'])) {
+if (isset($_POST['editSeries'])) {
 
     include('../db.php');
+    $id = $_GET['id'];
     $name = $_POST['name'];
     $genre = implode(", ", $_POST["genre"]);
     $realease = $_POST['realease'];
     $episode = $_POST['episode'];
     $season = $_POST['season'];
     $synopsis = $_POST['synopsis'];
-
     $query = mysqli_query(
         $con,
         "UPDATE series SET name = '$name', genre = '$genre', 
-        realease = '$realease',episode = '$episode',season = '$season', synopsis = '$synopsis' WHERE id='$id'"
+        realease = '$realease', episode = '$episode', season = '$season', synopsis = '$synopsis' WHERE id='$id'"
     )
         or die(mysqli_error($con)); 
 
     if ($query) {
         echo
         '<script>
-            alert("Edit Series Success!"); window.location = "../page/listSeriesPage.php"
+            alert("Edit Series Success"); window.location = "../page/listSeriesPage.php"
             </script>';
     } else {
         echo
         '<script>
-            alert("Edit Series Failed!");
+            alert("Edit Series Failed");
             </script>';
     }
 } else {
@@ -34,4 +34,3 @@ if (isset($_POST['edit'])) {
  window.history.back()
  </script>';
 }
-?>
